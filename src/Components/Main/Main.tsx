086232016653cc7a93c23ac474550a1a8b6a8eb9
metastasio <myaldzina.m@gmail.com>
2024-06-22 16:42:30 +0300

    Adding CSS styles

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -11,28 +11,39 @@ export const Main = () => {
           <source srcSet='img.png' media='(max-width: 600px)' />
           <img src='img big.png' alt='Фото, рекламирующее курс' />
         </picture>
-        <ul>
-          <li>
+        <ul className={styles.card_list}>
+          <li className={styles.card_item}>
             <CardItem />
           </li>
-          <li>
+          <li className={styles.card_item}>
             <CardItem />
           </li>
-          <li>
+          <li className={styles.card_item}>
             <CardItem />
           </li>
-          <li>
+          <li className={styles.card_item}>
             <CardItem />
           </li>
         </ul>
-        <p>
+        <p className={styles.text_hint}>
           Следуя плану на 3 месяца, люди получают в 2 раза лучший результат, чем
           за 1 месяц
         </p>
-        <label htmlFor='policy'>
-          Я соглашаюсь с Правилами сервиса и условиями Публичной оферты.
-        </label>
-        <input id='policy' type='checkbox' />
+        <div className={styles.policy}>
+          <input id='policy' type='checkbox' className={styles.policy_input} />
+          <label htmlFor='policy' className={styles.policy_label}>
+            Я соглашаюсь с <a href='#'>Правилами сервиса</a> и условиями{' '}
+            <a href='#'>Публичной оферты</a>.
+          </label>
+        </div>
+        <button className={styles.button_buy}>купить</button>
+
+        <p className={styles.policy_warning}>
+          Нажимая «Купить», Пользователь соглашается на автоматическое списание
+          денежных средств по истечению купленного периода. Дальнейшие списания
+          по тарифам участвующим в акции осуществляются по полной стоимости
+          согласно оферте.
+        </p>
       </main>
     </>
   );
