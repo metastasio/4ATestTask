b7249c03b6eba83d688464cd272d869df3f57a59
metastasio <myaldzina.m@gmail.com>
2024-06-25 20:37:20 +0300

    Fix context

--- src/useTimerContext.tsx
+++ src/useTimerContext.tsx
@@ -1,4 +1,10 @@
 import { useContext } from 'react';
 import { TimerContext } from './contextProvider';
 
-export const useTimerContext = () => useContext(TimerContext);
+export const useTimerContext = () => {
+  const context = useContext(TimerContext);
+  if (!context) {
+    throw new Error('Context must be provided');
+  }
+  return context;
+};
