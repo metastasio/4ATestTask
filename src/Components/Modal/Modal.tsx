5b8f505168e2e48196f77e84f92ed85b9574182e
metastasio <myaldzina.m@gmail.com>
2024-06-25 20:16:46 +0300

    Applying styles to Modal

--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -8,22 +8,31 @@ export const Modal = ({ setIsOpen }: ModalProps) => {
   return (
     <>
       <div className={styles.background} onClick={() => setIsOpen(false)} />
-      {/* <div className={styles.centered}> */}
-        <div className={styles.modal}>
-          
-          <div className={styles.modalHeader}>
-            <h2 className={styles.heading}>
-              не упусти свой шанс <br />
-              <span>последний шанс</span>
-            </h2>
-          </div>
-          <p>
-            Мы знаем, как трудно начать.. <span>Поэтому!</span>
-          </p>
-          <div>Дарим скидку для лёгкого старта 🏃‍♂️</div>
-          <p>Посмотри, что мы для тебя приготовили 🔥</p>
-          <p>ахахха ничего</p>
+      <div className={styles.modal}>
+        <div className={styles.label}>горящее предложение</div>
+        <button
+          className={styles.button_close}
+          onClick={() => setIsOpen(false)}
+        ></button>
+        {/* <div className={styles.heading}> */}
+        <h2 className={styles.header}>
+          не упусти свой шанс <br />
+          <span className={styles.accent}>последний шанс</span>
+        </h2>
         {/* </div> */}
+
+        <p className={styles.text}>
+          Мы знаем, как трудно начать.. <strong>Поэтому!</strong>
+        </p>
+        <p className={styles.bordered_p}>
+          <strong>
+            Дарим скидку для{' '}
+            <span className={styles.accent}>лёгкого старта</span>
+          </strong>
+          🏃‍♂️
+        </p>
+        <p className={styles.text}>Посмотри, что мы для тебя приготовили 🔥</p>
+        <p>ахахха ничего</p>
       </div>
     </>
   );
