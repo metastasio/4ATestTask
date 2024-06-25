f778acad3ee89a0890f9652449875234fef6b174
metastasio <myaldzina.m@gmail.com>
2024-06-25 12:31:04 +0300

    Add .expiring

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,15 +1,15 @@
+import cn from 'classnames';
 import { useEffect, useState } from 'react';
-// import { TimerContext } from '../../App';
+import { useTimerContext } from '../../useTimerContext';
 
 import styles from './header.module.css';
-import { useTimerContext } from '../../useTimerContext';
 
 export const Header = () => {
   //   Получение данных от сервиса
   // Ссылка на получение: https://t-pay.iqfit.app/subscribe/list-test
   // Пример одной записи:
-  const isExpiring = useTimerContext();
-  console.log(isExpiring);
+  const { status, setStatus } = useTimerContext();
+  console.log(status);
   const [time, setTime] = useState(120000);
   const currentHours = Math.floor((time / (1000 * 60 * 60)) % 24);
   const currentMinutes = Math.floor((time / 1000 / 60) % 60);
@@ -22,28 +22,36 @@ export const Header = () => {
           setTime((prev) => (prev -= 1000));
         }
         if (time <= 30000) {
-          console.log('hehe');
+          setStatus('expiring');
         }
       };
       getTime();
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [currentHours, currentMinutes, time]);
+  }, [currentHours, currentMinutes, time, setStatus]);
 
   return (
     <header className={styles.header}>
       <p className={styles.text}>Скидка действует:</p>
       <div className={styles.timer_wrapper}>
         <div className={styles.timer_unit_wrapper}>
-          <p className={styles.timer_time}>
+          <p
+            className={cn(styles.timer_time, {
+              [styles.expiring]: status === 'expiring',
+            })}
+          >
             {`${currentHours}`.padStart(2, '0')}
           </p>
           <p className={styles.timer_text}>часов</p>
         </div>
         <p className={styles.timer_separator}>:</p>
         <div className={styles.timer_unit_wrapper}>
-          <p className={styles.timer_time}>
+          <p
+            className={cn(styles.timer_time, {
+              [styles.expiring]: status === 'expiring',
+            })}
+          >
             {`${currentMinutes}`.padStart(2, '0')}
           </p>
           <p className={styles.timer_text}>минут</p>
