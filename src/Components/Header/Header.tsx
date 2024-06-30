49ff338ead8dc72800e08a1c2911a7992e4fe8ca
metastasio <myaldzina.m@gmail.com>
2024-06-30 21:13:59 +0300

    Fix font import

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -6,7 +6,7 @@ import styles from './header.module.css';
 
 export const Header = () => {
   const { status, setStatus } = useTimerContext();
-  const [time, setTime] = useState(1000);
+  const [time, setTime] = useState(1220000);
   const currentMinutes = Math.floor((time / 1000 / 60) % 60);
   const currentSeconds = Math.floor((time / 1000) % 60);
 
